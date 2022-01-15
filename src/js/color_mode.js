d883a072679924f3c074b888da6dc48a99be3c72
Szymon Okulowski <szymon.okulowski@gmail.com>
2022-01-15 21:22:10 +0100

    create project

---
+++ src/js/color_mode.js
@@ -0,0 +1,30 @@
+const switchBtn = document.querySelector('.switch')
+const switchInside = document.querySelector('.switch-inside')
+const body = document.querySelector('body')
+const header = document.querySelector('.container__header')
+
+let mode = localStorage.getItem('mode')
+
+const checkMode = () => {
+	if (mode === 'dark') {
+		body.dataset.mode === 'dark'
+		changeMode()
+	}
+}
+
+const changeMode = () => {
+	if (body.dataset.mode === 'light') {
+		body.dataset.mode = 'dark'
+		switchInside.classList.add('switch-active')
+		header.style.backgroundImage = "url('../../dist/img/night.jpg')"
+		localStorage.setItem('mode', 'dark')
+	} else {
+		body.dataset.mode = 'light'
+		switchInside.classList.remove('switch-active')
+		header.style.backgroundImage = "url('../../dist/img/day.jpg')"
+		localStorage.setItem('mode', 'light')
+	}
+}
+
+window.addEventListener('DOMContentLoaded', checkMode)
+switchBtn.addEventListener('click', changeMode)
---
+++ src/js/weather_api.js
@@ -0,0 +1,8 @@
+const input = document.querySelector('.container__search-input')
+const btn = document.querySelector('.container__search-btn')
+const cityName = document.querySelector('.container__city')
+const statusImg = document.querySelector('.container__status-img')
+const degree = document.querySelector('.degree')
+
+const API_LINK = 'https://pro.openweathermap.org/data/2.5/forecast/hourly?q='
+const API_KEY = '3398394920a3ba1d4086e36266ddd263'
