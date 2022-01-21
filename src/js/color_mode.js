b409f6b2dc432e00d961cc46f054ae918db4f67d
Szymon Okulowski <szymon.okulowski@gmail.com>
2022-01-21 18:37:10 +0100

    add prettier

--- src/js/color_mode.js
+++ src/js/color_mode.js
@@ -1,33 +1,32 @@
-const switchBtn = document.querySelector('.switch')
-const switchInside = document.querySelector('.switch-inside')
-const body = document.querySelector('body')
-const header = document.querySelector('.container__header')
-const arrow = document.querySelector('.arrow')
+const switchBtn = document.querySelector('.switch');
+const switchInside = document.querySelector('.switch-inside');
+const body = document.querySelector('body');
+const header = document.querySelector('.container__header');
+const arrow = document.querySelector('.arrow');
 
-let mode = localStorage.getItem('mode')
-
-const checkMode = () => {
-	if (mode === 'dark') {
-		body.dataset.mode === 'dark'
-		changeMode()
-	}
-}
+const mode = localStorage.getItem('mode');
 
 const changeMode = () => {
-	if (body.dataset.mode === 'light') {
-		body.dataset.mode = 'dark'
-		switchInside.classList.add('switch-active')
-		header.style.backgroundImage = "url('./dist/img/night.jpg')"
-		arrow.setAttribute('src', './dist/img/arrow-down-dark.svg')
-		localStorage.setItem('mode', 'dark')
-	} else {
-		body.dataset.mode = 'light'
-		switchInside.classList.remove('switch-active')
-		header.style.backgroundImage = "url('./dist/img/day.jpg')"
-		arrow.setAttribute('src', './dist/img/arrow-down-light.svg')
-		localStorage.setItem('mode', 'light')
-	}
-}
+  if (body.dataset.mode === 'light') {
+    body.dataset.mode = 'dark';
+    switchInside.classList.add('switch-active');
+    header.style.backgroundImage = "url('./dist/img/night.jpg')";
+    arrow.setAttribute('src', './dist/img/arrow-down-dark.svg');
+    localStorage.setItem('mode', 'dark');
+  } else {
+    body.dataset.mode = 'light';
+    switchInside.classList.remove('switch-active');
+    header.style.backgroundImage = "url('./dist/img/day.jpg')";
+    arrow.setAttribute('src', './dist/img/arrow-down-light.svg');
+    localStorage.setItem('mode', 'light');
+  }
+};
 
-window.addEventListener('DOMContentLoaded', checkMode)
-switchBtn.addEventListener('click', changeMode)
+const checkMode = () => {
+  if (mode === 'dark') {
+    changeMode();
+  }
+};
+
+window.addEventListener('DOMContentLoaded', checkMode);
+switchBtn.addEventListener('click', changeMode);
